531a7856abd6db3e721c00776d141a5b6e648efc
Boitumelo Lefaphana <boitumelolefaphana100@gmail.com>
2024-05-08 11:49:18 +0200

    updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ProjectsService } from '../_services/projects.service';
+import { Project } from '../models/Projects';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  constructor(private titleService: Title){
-    this.titleService.setTitle('Boitumelo Lefaphana - Home')
+export class HomeComponent implements OnInit {
+  featuredProject = {} as Project;
+  constructor(
+    private titleService: Title,
+    private projectService: ProjectsService
+  ) {
+    this.titleService.setTitle('Boitumelo Lefaphana - Home');
   }
-
+  ngOnInit(): void {
+    this.featuredProject = this.projectService.GetProjectById(0);
+  }
 }
