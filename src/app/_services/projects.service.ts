1dadac3ca4a137e8b206430aef569e5f8f4887e5
Boitumelo Lefaphana <boitumelolefaphana100@gmail.com>
2024-05-10 11:17:02 +0200

    updates

--- src/app/_services/projects.service.ts
+++ src/app/_services/projects.service.ts
@@ -16,24 +16,24 @@ export class ProjectsService {
         '../../assets/snake3.gif',
       ],
       projectLink: 'https://github.com/Boitumelo-Lefaphana/Snake-Game',
-      summary: 'Python project that analyzes stock market data.',
+      summary: 'Java project that stimulates classical snake game.',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       tags: [Tag.JAVA],
     },
     {
       id: 1,
-      name: 'Sample Angular App',
+      name: 'Angular Portfolio Website',
       pictures: [
         '../../assets/image1.png',
         '../../assets/image2.png',
         '../../assets/image3.png',
       ],
-      projectLink: '//www.github.com',
-      summary: 'Fullstack web app developed using Angular and Node.JS',
+      projectLink: 'https://github.com/Boitumelo-Lefaphana/Portolio-Website',
+      summary: 'FrondEnd portfolio using Angular and TypeScript',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      tags: [Tag.ANGULAR, Tag.TYPESCRIPT, Tag.NODEJS],
+      tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
     },
     {
       id: 2,
